a809582fd6ad5bdbe03ceb67fa746177e0aaf478
Peter Andersson <pjotor87@hotmail.com>
2020-08-06 15:39:20 +0200

    Refactoring

--- tests/test_seed-db.py
+++ tests/test_seed-db.py
@@ -2,31 +2,30 @@
 
 
 import pytest
-import djangoapp.models as Models
+from djangoapp.models import Musician as Model
 import djangoapp.seed as Seeder
 
 
 @pytest.mark.django_db
 class Test_Seed():
+    """Should test seed functionality."""
 
     def test_should_find_import_of_model_without_crashing_when_running_from_root(self):
-        """Should."""
         assert Seeder.SQLiteSeed is not None, "SQLiteSeed could not be found, should exist in seed.py"
 
     def test_should_seed_the_musician_table(self):
-        """Should."""
         testdata_seed = [
-            Models.Musician(firstname="James", lastname="Hetfield", band="Metallica"),
-            Models.Musician(firstname="John", lastname="Petrucci", band="Dream Theater"),
-            Models.Musician(firstname="Mike", lastname="Portnoy", band="Dream Theater"),
-            Models.Musician(firstname="Dave", lastname="Mustaine", band="Megadeth"),
+            Model(firstname="James", lastname="Hetfield", band="Metallica"),
+            Model(firstname="John", lastname="Petrucci", band="Dream Theater"),
+            Model(firstname="Mike", lastname="Portnoy", band="Dream Theater"),
+            Model(firstname="Dave", lastname="Mustaine", band="Megadeth"),
         ]
         seeder = Seeder.SQLiteSeed(testdata_seed)
         seeder.seed()
 
         try:
             # Expected behaviour is to fail here and raise exception
-            Models.Musician.objects.get(firstname="Non", lastname="Existing", band="Entry")
-        except Models.Musician.DoesNotExist:
+            Model.objects.get(firstname="Non", lastname="Existing", band="Entry")
+        except Model.DoesNotExist:
             pass
-            assert Models.Musician.objects.get(firstname="Mike", lastname="Portnoy", band="Dream Theater") is not None, "Did not find A seeded entry that should exist"
+            assert Model.objects.get(firstname="Mike", lastname="Portnoy", band="Dream Theater") is not None, "Did not find A seeded entry that should exist"
