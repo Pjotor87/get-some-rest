5a9b44b99af2600105de3ef1e15c5d13a33da56b
Peter Andersson <pjotor87@hotmail.com>
2020-08-06 15:33:44 +0200

    Refactoring

--- tests/test_seed-db.py
+++ tests/test_seed-db.py
@@ -6,26 +6,27 @@ import djangoapp.models as Models
 import djangoapp.seed as Seeder
 
 
-def test_should_find_import_of_model_without_crashing_when_running_from_root():
-    """Should."""
-    assert Seeder.SQLiteSeed is not None, "SQLiteSeed could not be found, should exist in seed.py"
-
-
 @pytest.mark.django_db
-def test_should_seed_the_musician_table():
-    """Should."""
-    testdata_seed = [
-        Models.Musician(firstname="James", lastname="Hetfield", band="Metallica"),
-        Models.Musician(firstname="John", lastname="Petrucci", band="Dream Theater"),
-        Models.Musician(firstname="Mike", lastname="Portnoy", band="Dream Theater"),
-        Models.Musician(firstname="Dave", lastname="Mustaine", band="Megadeth"),
-    ]
-    seeder = Seeder.SQLiteSeed(testdata_seed)
-    seeder.seed()
+class Test_Seed():
 
-    try:
-        # Expected behaviour is to fail here and raise exception
-        Models.Musician.objects.get(firstname="Non", lastname="Existing", band="Entry")
-    except Models.Musician.DoesNotExist:
-        pass
-    assert Models.Musician.objects.get(firstname="Mike", lastname="Portnoy", band="Dream Theater") is not None, "Did not find A seeded entry that should exist"
+    def test_should_find_import_of_model_without_crashing_when_running_from_root(self):
+        """Should."""
+        assert Seeder.SQLiteSeed is not None, "SQLiteSeed could not be found, should exist in seed.py"
+
+    def test_should_seed_the_musician_table(self):
+        """Should."""
+        testdata_seed = [
+            Models.Musician(firstname="James", lastname="Hetfield", band="Metallica"),
+            Models.Musician(firstname="John", lastname="Petrucci", band="Dream Theater"),
+            Models.Musician(firstname="Mike", lastname="Portnoy", band="Dream Theater"),
+            Models.Musician(firstname="Dave", lastname="Mustaine", band="Megadeth"),
+        ]
+        seeder = Seeder.SQLiteSeed(testdata_seed)
+        seeder.seed()
+
+        try:
+            # Expected behaviour is to fail here and raise exception
+            Models.Musician.objects.get(firstname="Non", lastname="Existing", band="Entry")
+        except Models.Musician.DoesNotExist:
+            pass
+            assert Models.Musician.objects.get(firstname="Mike", lastname="Portnoy", band="Dream Theater") is not None, "Did not find A seeded entry that should exist"
