4327a82525b73b85720b23fa3a6cbc84c4c4fbd9
Peter Andersson <pjotor87@hotmail.com>
2020-08-09 22:15:58 +0200

    Test for API call

--- angularapp/cypress/integration/angularapp/musicians.spec.js
+++ angularapp/cypress/integration/angularapp/musicians.spec.js
@@ -15,4 +15,11 @@ context('Location', () => {
   it('should print out a message that the new component works', () => {
     cy.get('app-musicians > p').should('contain', 'works!');
   });
+
+  it('gets musicians from api and displays on the page', () => {
+    cy.get('app-musicians div').should('contain', 'n/a');
+    cy.log('Waiting one second for api call to return...');
+    cy.wait(1000);
+    cy.get('app-musicians div').should('contain', 'Hetfield');
+  });
 });
